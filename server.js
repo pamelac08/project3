39af7eec41a4b16cb4e53c1d3c738a21d5d5d79f
mitchgreene92 <mitchgreene92@gmail.com>
2020-06-16 21:31:15 -0500

    adding initial user auth files

--- server.js
+++ server.js
@@ -4,7 +4,58 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require("./app/models");
+const Role = db.role;
 
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +67,8 @@ if (process.env.NODE_ENV === "production") {
 
 
 app.use(routes);
-
+require('../project3/routes/api/auth/auth.routes')(app);
+require('../project3/routes/api/auth/user.routes')(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/autocoach");
 
