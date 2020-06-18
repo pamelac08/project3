337583ef061d676c85c836f76c042f6a91ba60f7
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-06-17 20:14:41 -0500

    removed css file

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
-import "./Login.css"
 
 class LoginForm extends Component {
 
