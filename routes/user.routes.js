d5e9ec097d7636d732294eca73634ad1134f2f4e
mitchgreene92 <mitchgreene92@gmail.com>
2020-06-19 11:44:37 -0500

    fixed some typos, incorrect requirements  and added a db.config file

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,4 @@
-const { authJwt } = require("../middlewares");
+const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
