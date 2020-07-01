cfa940b5603595acbd0ab9e0ee2be6dbdd2a705f
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-06-30 20:09:20 -0500

    missing picture issue

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Image } from "react-bootstrap";
 
 function Header() {
-  return <Image src="fitness.png" width={1500} height={500} />;
+  return <Image src="pic.png" width={1500} height={500} />;
 }
 
 export default Header;
