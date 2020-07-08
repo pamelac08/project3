ce49eddd18fd2733c04e08e9f75f8ba1f67f9d6d
pamelac08 <pamelac08@gmail.com>
2020-07-07 20:41:03 -0500

    updating client side api routes

--- client/src/utils/LoginAPI.js
+++ client/src/utils/LoginAPI.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 export default {
   createUser: function (newUser) {
-    return axios.post("http://localhost:3001/api/auth/signup", newUser);
+    return axios.post("/api/auth/signup", newUser);
   },
   loginUser: function (user) {
-    return axios.post("http://localhost:3001/api/auth/signin", user);
+    return axios.post("/api/auth/signin", user);
   },
   deleteUser: function (id) {
-    return axios.delete("http://localhost:3001/api/coach/users/" + id);
+    return axios.delete("/api/coach/users/" + id);
   },
   updateUser: function (id, updatedUser) {
-    return axios.put("http://localhost:3001/api/coach/users/" + id, updatedUser);
+    return axios.put("/api/coach/users/" + id, updatedUser);
   },
   getAllUsers: function () {
-    return axios.get("http://localhost:3001/api/users")
+    return axios.get("/api/users")
   }
 };
